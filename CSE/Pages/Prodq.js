68bb6e06603078f7408761f307bf5eb3df3b2fd4
Merrouki Yacine <120087438+YacineMK@users.noreply.github.com>
2023-04-12 23:42:38 +0100

    Add files via upload

---
+++ CSE/Pages/Prodq.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './ProductCards.css';
+import cas from './Podu1ct.png';
+ 
+function JD() {
+    return (
+     <div>
+        <h1>Polaire product</h1>
+      <div className="card">
+          <div>
+           <img src={cas} />
+           <div className='txtl'>
+           <h4>ASUS mouse RGB</h4>
+           <p>2000DA</p>
+           </div>
+          </div>
+          <div>
+           <img src={cas} />
+           <div className='txtl'>
+           <h4>ASUS mouse RGB</h4>
+           <p>2000DA</p>
+           </div>
+          </div> <div>
+           <img src={cas} />
+           <div className='txtl'>
+           <h4>ASUS mouse RGB</h4>
+           <p>2000DA</p>
+           </div>
+          </div> <div>
+           <img src={cas} />
+           <div className='txtl'>
+           <h4>ASUS mouse RGB</h4>
+           <p>2000DA</p>
+           </div>
+          </div> <div>
+           <img src={cas} />
+           <div className='txtl'>
+           <h4>ASUS mouse RGB</h4>
+           <p>2000DA</p>
+           </div>
+          </div> <div>
+           <img src={cas} />
+           <div className='txtl'>
+           <h4>ASUS mouse RGB</h4>
+           <p>2000DA</p>
+           </div>
+          </div> <div>
+           <img src={cas} />
+           <div className='txtl'>
+           <h4>ASUS mouse RGB</h4>
+           <p>2000DA</p>
+           </div>
+          </div> <div>
+           <img src={cas} />
+           <div className='txtl'>
+           <h4>ASUS mouse RGB</h4>
+           <p>2000DA</p>
+           </div>
+          </div> 
+      </div>
+      </div>
+    );
+  }
+  
+  export default JD;
